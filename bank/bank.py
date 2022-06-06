fbe620b2bc6cfda5504212b1f56eb1d7d6a5bbf1
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-06 19:55:45 +0000

    Mon, Jun 6, 2022, 7:55 PM +00:00

--- bank/bank.py
+++ bank/bank.py
@@ -1,2 +1,9 @@
 greeting = input ("Greeting: ").strip().lower()
 print(greeting)
+
+if greeting.startswith("hello"):
+    print("$0")
+elif greeting.startswith("h"):
+    print("$20")
+else:
+    print("$100")
