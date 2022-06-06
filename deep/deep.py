f02f4a0accfc28753f06057a1a26c5377d9e7801
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-06 19:33:03 +0000

    Mon, Jun 6, 2022, 7:33 PM +00:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,2 +1,14 @@
 answer= input ("What is the answer to the Great Question of Life, the Universe and Everything? ")
-print(answer)
+
+answer.lower()
+if answer == "42":
+    response ="YES"
+elif answer == "forty-two":
+    response ="YES"
+elif answer == "forty two":
+    response ="YES"
+else:
+    response ="No"
+
+print(response)
+
