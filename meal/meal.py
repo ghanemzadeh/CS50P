d7f3767d7bd9088dd40afaad7bc94091cee1da2d
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-07 14:08:11 +0000

    Tue, Jun 7, 2022, 2:08 PM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,1 +1,11 @@
-time = input("What time is it? ")
+def main():
+    time = input("What time is it? ")
+    convert(time)
+
+
+def convert(time):
+    ...
+
+
+if __name__ == "__main__":
+    main()
