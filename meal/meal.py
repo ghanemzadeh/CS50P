6c425423dab01a0f8697cdd58a55312bd33c61f1
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-07 14:23:35 +0000

    Tue, Jun 7, 2022, 2:23 PM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -2,14 +2,17 @@ def main():
     time = input("What time is it? ")
 
     coverted = convert(time)
-    
+
     if 7.0 <= coverted <= 8.0:
         print("breakfast time")
 
 
 def convert(time):
     h , m = time.split(":")
-    print (h,m)
+    h = float (h)
+    m = float (m)
+    print ("h+ (m*10/6) =", h+ (m*10/6))
+
     return h+ (m*10/6)
 
 
