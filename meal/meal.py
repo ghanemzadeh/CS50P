2bc2a11f3cc8558e91b84d194fad9a9dd93dbe53
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-07 14:26:20 +0000

    Tue, Jun 7, 2022, 2:26 PM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -11,9 +11,9 @@ def convert(time):
     h , m = time.split(":")
     h = float (h)
     m = float (m)
-    print ("h+ (m/60) =", h+ (m/6))
+    print ("h, m, h+ (m/60) =", h, m ,h+ (m/60))
 
-    return h+ (m/6)
+    return h+ (m/60)
 
 
 if __name__ == "__main__":
