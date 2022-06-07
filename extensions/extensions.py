3dffeabe3358e413b75934837f3bb40a6136580d
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-07 13:38:13 +0000

    Tue, Jun 7, 2022, 1:38 PM +00:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -4,5 +4,7 @@ if filename.endswith("gif"):
     print("image/gif")
 elif filename.endswith("jpg") or filename.endswith("jpeg"):
     print("image/jpg")
+elif filename.endswith("png"):
+    print("image/png")
 else:
     print ("application/octet-stream")
