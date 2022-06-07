4c7e58c13a7f7676ddfaa495aedb5ddf7a86411a
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-07 13:37:02 +0000

    Tue, Jun 7, 2022, 1:37 PM +00:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -2,5 +2,7 @@ filename = input ("File name: ").strip().lower()
 
 if filename.endswith("gif"):
     print("image/gif")
+elif filename.endswith("jpg") or filename.endswith("jpeg"):
+    print("image/jpg")
 else:
     print ("application/octet-stream")
