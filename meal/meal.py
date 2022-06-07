edc4a4adfd7d32930b13fbf1fcbb39b7c02ecc75
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-07 14:29:26 +0000

    Tue, Jun 7, 2022, 2:29 PM +00:00

--- meal/meal.py
+++ meal/meal.py
@@ -5,6 +5,10 @@ def main():
 
     if 7.0 <= coverted <= 8.0:
         print("breakfast time")
+    elif 12.0 <= coverted <= 13.0:
+        print("lunch time")
+    elif 18.0 <= coverted <= 19.0:
+        print("dinner time")
 
 
 def convert(time):
