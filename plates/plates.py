17568c34f2d33c288dcf5c85563fef1d1ce438a9
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-09 15:26:53 +0000

    Thu, Jun 9, 2022, 3:26 PM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -15,17 +15,14 @@ def is_valid(s):
         for c in s[2:len(s)]:
             i = s.index(c)
             if c in [" ", ",", ".","\"","'","?",";",":","!","-"]:
-                print("was it here")
                 status = False
                 break
             elif s[i:].isdigit() and int(c) != 0:
                 status = True
                 break
             else:
-                print("was it here, for else")
                 status = False
     else:
-        print("was it here, last else")
         status = False
 
     return status
