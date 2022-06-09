07fd6c6dd78ab9dafef55020d96e273ef2525ec5
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-09 16:38:17 +0000

    Thu, Jun 9, 2022, 4:38 PM +00:00

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,6 +1,6 @@
 fruits = {
     "apple": 130,
-    "avocado california": 50,
+    "avocado": 50,
     "banana": 110,
     "cantaloupe": 50,
     "grapefruit": 60,
@@ -16,7 +16,7 @@ fruits = {
     "pineapple": 50,
     "plums": 70,
     "ptrawberries": 50,
-    "Sweet cherries": 100,
+    "sweet cherries": 100,
     "tangerine": 50,
     "watermelon": 80
 }
