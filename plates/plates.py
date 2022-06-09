cf47b98f08da1b0fa44e5160dc5b4029a1ce7bb8
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-09 15:23:35 +0000

    Thu, Jun 9, 2022, 3:23 PM +00:00

--- plates/plates.py
+++ plates/plates.py
@@ -11,18 +11,20 @@ def is_valid(s):
     if len(s) < 2 or len(s) > 6 :
         status = False
     elif s[0:2].isalpha():
-        
+
         for c in s[2:len(s)]:
             i = s.index(c)
             if c in [" ", ",", ".","\"","'","?",";",":","!","-"]:
                 print("was it here")
                 status = False
                 break
-            #elif s[i:].isdigit() and int(c) != 0:
-             #   break
+            elif s[i:].isdigit() and int(c) != 0:
+                return True
             else:
+                print("was it here, for else")
                 status = False
     else:
+        print("was it here, last else")
         status = False
 
     return status
