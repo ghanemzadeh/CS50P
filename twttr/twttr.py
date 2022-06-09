89cb5aba595179b67010469937b85815a6a37293
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-09 12:10:52 +0000

    Thu, Jun 9, 2022, 12:10 PM +00:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -2,7 +2,7 @@ string = input ("Input: ")
 
 new_str = ""
 for c in string:
-    if c in [A, a, e, E, i, I, o, O, u, U]:
+    if c in ["A", "a", "e", "E", "i", "I", "o", "O", "u", "U"]:
         continue
     new_str += c
 print (new_str)
