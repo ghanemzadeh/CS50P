d17c0d1180bb6968180eba96e9aea50f4b942f84
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-09 11:57:33 +0000

    Thu, Jun 9, 2022, 11:57 AM +00:00

--- coke/coke.py
+++ coke/coke.py
@@ -2,9 +2,9 @@ due = 50
 while due> 0:
     print("Amount Due: ", due)
     coin = int(input("insert coin: "))
-    #if coin in [5, 10, 25]:
-    if coin > due:
-        change = coin - due
-    due-= coin
+    if coin in [5, 10, 25]:
+        if coin > due:
+            change = coin - due
+        due-= coin
 
 print ("Change owed: ", change)
