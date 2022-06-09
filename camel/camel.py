e42dcf8df28b94761490d5792d466ab0c043d004
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-09 11:16:40 +0000

    Thu, Jun 9, 2022, 11:16 AM +00:00

--- camel/camel.py
+++ camel/camel.py
@@ -3,6 +3,6 @@ string = input ("camelCase: ")
 
 for c in string:
     if c.isupper():
-        print("_", c.lower(), end=" ")
+        print("_", c.lower(), end="")
     else:
         print(c, end="")
