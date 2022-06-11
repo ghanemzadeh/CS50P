79d1c3bb3e8c8d22290f5f332d1c633bacd71b0a
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-11 11:47:24 +0000

    Sat, Jun 11, 2022, 11:47 AM +00:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -3,16 +3,16 @@ x, y = fraction.split("/")
 x = int(x)
 y = int(y)
 
-try:
-
-    percent = int(x/y*100)
-    if percent <= 1:
-        print("E")
-    elif percent >= 90:
-        print("F")
-    else:
-        print (str(percent)+"%")
-except ValueError:
-    print("x/y, x or y are not intger")
-except ZeroDivisionError:
-    print("x/y, y should not be zero")
+while True:
+    try:
+        percent = int(x/y*100)
+        if percent <= 1:
+           print("E")
+        elif percent >= 90:
+            print("F")
+        else:
+            print (str(percent)+"%")
+    except ValueError:
+        pass
+    except ZeroDivisionError:
+        pass
