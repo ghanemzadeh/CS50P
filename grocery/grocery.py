dec423ca538a5fb2ead96e875e7ff5de6fd3bdae
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-11 14:19:32 +0000

    Sat, Jun 11, 2022, 2:19 PM +00:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -11,12 +11,11 @@ while True:
         else:
             groceries.update({grocery:1})
     except EOFError:
-        print("\n")
         break
     except KeyError:
         pass
 
-print (groceries)
+#print (groceries)
 
 for grocery in sorted (groceries.keys()):
     print(groceries[grocery], grocery)
