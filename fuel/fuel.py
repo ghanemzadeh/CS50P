93eb4ebdd1575cb426b54dc9385d984dec4c9668
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-11 11:35:03 +0000

    Sat, Jun 11, 2022, 11:35 AM +00:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -3,7 +3,7 @@ x, y = fraction.split("/")
 try:
     x = int(x)
     y = int(y)
-    print (x,y)
+    print (int(x/y*100),"%")
 except ValueError:
     print("x/y, x or y are not intger")
 except ZeroDivisionError:
