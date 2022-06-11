f3b9f4607fd314b9f5ed17fcd899496ff0519844
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-11 11:31:06 +0000

    Sat, Jun 11, 2022, 11:31 AM +00:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,6 +1,10 @@
 fraction = input ("Fraction: ")
-x, y = expression.split("/")
+x, y = fraction.split("/")
 try:
     x = int(x)
     y = int(y)
     print (x,y)
+except ValueError:
+    print("x/y, x or y are not intger")
+except ZeroDivisionError:
+    print("x/y, y should not be zero")
