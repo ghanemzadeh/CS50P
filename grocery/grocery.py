3635bc4c8e8a1aab718b3c4d34cbcfbba9fbb49b
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-11 14:38:41 +0000

    Sat, Jun 11, 2022, 2:38 PM +00:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -2,12 +2,13 @@ groceries = {}
 
 while True:
     try:
-        grocery = input("Item: ").upper()
+        grocery = input().upper()
         if grocery in groceries:
             groceries[grocery]+=1
         else:
             groceries[grocery]=1
     except EOFError:
+        print("\n")
         for grocery in sorted (groceries.keys()):
             print(groceries[grocery], grocery)
         break
