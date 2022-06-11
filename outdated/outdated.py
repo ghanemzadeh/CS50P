3b88f02caab3781ec53e2262b231ef340379fb76
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-11 15:45:11 +0000

    Sat, Jun 11, 2022, 3:45 PM +00:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -22,7 +22,7 @@ while True:
        #0 >= m >= 13 or
        if d > 31 or m >12:
            continue
-       print (y, f"{m:02}", d, sep="-", end="")
+       print (y, f"{m:02}", f"{d:02}", sep="-", end="")
        break
     except ValueError:
         try:
@@ -30,7 +30,7 @@ while True:
             m = months.index(m) + 1
             if d > 31 or m >12:
                 continue
-            print (y, f"{m:02}", d, sep="-")
+            print (y, f"{m:02}", f"{d:02}", sep="-", end="")
             break
         except ValueError:
             pass
