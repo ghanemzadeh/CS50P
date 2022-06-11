0921e9e4d7810e8cb211cf553d6056d3e04ed5d5
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-11 13:41:11 +0000

    Sat, Jun 11, 2022, 1:41 PM +00:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -6,10 +6,12 @@ while True:
 
     try:
         item = input("Item: ").title()
-        if grocery in items:
+        if grocery in groceries:
             price = float (items[item])
             total +=round(price, 2)
             print("total: ", f"${total:.2f}")
+        else:
+            
     except EOFError:
         print("\n")
         break
