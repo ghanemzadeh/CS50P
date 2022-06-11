3f5a7b4e2d43856259085890e9c033dcd95178b6
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-11 13:45:37 +0000

    Sat, Jun 11, 2022, 1:45 PM +00:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -5,8 +5,9 @@ total = 0
 while True:
 
     try:
-        grocery = input("Item: ").title()
+        grocery = input("Item: ").upper()
         if grocery in groceries:
+            groceries.update({grocery:})
             groceries[grocery]+=1
             total +=round(price, 2)
             print("total: ", f"${total:.2f}")
