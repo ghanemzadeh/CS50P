a51a70e5f9d326bcdc50db95e37bbcb6b961f4ca
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-11 13:47:08 +0000

    Sat, Jun 11, 2022, 1:47 PM +00:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -7,14 +7,14 @@ while True:
     try:
         grocery = input("Item: ").upper()
         if grocery in groceries:
-            groceries.update({grocery:})
             groceries[grocery]+=1
             total +=round(price, 2)
             print("total: ", f"${total:.2f}")
         else:
-
+            groceries.update({grocery:1})
     except EOFError:
         print("\n")
         break
     except KeyError:
         pass
+print(groceries)
