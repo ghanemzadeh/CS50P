cb7af243a677405acb86d4ab5adf2295a36a39f6
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-15 10:32:24 +0000

    Wed, Jun 15, 2022, 10:32 AM +00:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -1,1 +1,11 @@
 name = input ("Name: ")
+
+while True:
+    try:
+        grocery = input().upper()
+        if grocery in groceries:
+            groceries[grocery]+=1
+        else:
+            groceries[grocery]=1
+    except EOFError:
+        print("\n")
