96871a1b253f37372d287723f2cc950e58fd9ca9
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-15 05:42:54 +0000

    Wed, Jun 15, 2022, 5:42 AM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -2,13 +2,14 @@ import sys
 from pyfiglet import Figlet
 
 text = input("Input: ")
+figlet = Figlet()
 
 if len(sys.argv) == 3:
     if sys.argv[1] == "-f" or sys.argv[1] == "--font":
         f = sys.argv[2]
 elif len(sys.argv) == 1:
+    print ("fonts: ",figlet.getFonts())
     f = "slant"
 
-figlet = Figlet()
 figlet.setFont(font=f)
 print(figlet.renderText(text))
