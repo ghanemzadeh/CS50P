929327e7506f43375c1c98fd0973e25a441f5ad4
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-15 06:14:04 +0000

    Wed, Jun 15, 2022, 6:14 AM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -2,7 +2,7 @@ import sys
 from pyfiglet import Figlet
 from random import choice
 
-text = input("Input: ")
+
 figlet = Figlet()
 
 if len(sys.argv) == 3:
@@ -11,16 +11,12 @@ if len(sys.argv) == 3:
         if f not in figlet.getFonts():
             sys.exit("Invalid usage")
     else:
-        print("point 2")
         sys.exit("Invalid usage")
-
 elif len(sys.argv) == 1:
     f = choice(figlet.getFonts())
-    print("point 3")
 else:
-    print("point 4")
     sys.exit("Invalid usage")
 
-
+text = input("Input: ")
 figlet.setFont(font=f)
 print(figlet.renderText(text))
