12d706807c4e326a5e14ba2f4f5769c70918faaa
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-15 06:08:19 +0000

    Wed, Jun 15, 2022, 6:08 AM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -8,7 +8,9 @@ figlet = Figlet()
 if len(sys.argv) == 3:
     if sys.argv[1] == "-f" or sys.argv[1] == "--font":
         f = sys.argv[2]
-        print("point 1")
+        if figlet.FontNotFound:
+            print("point 1")
+            sys.exit("Invalid usage")
     else:
         print("point 2")
         sys.exit("Invalid usage")
