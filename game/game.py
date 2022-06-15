44f05320490cbd85b9c1a6d8d54875c617e057dc
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-15 11:42:17 +0000

    Wed, Jun 15, 2022, 11:42 AM +00:00

--- game/game.py
+++ game/game.py
@@ -4,17 +4,20 @@ while True:
     level = int(input("Level: "))
     if level <= 0:
         pass
+    break
 
-    guess = randrange(1,level+1)
+guess = randrange(1,level+1)
 
-    while True:
-        try:
-            uguess = int(input("Guess: "))
-         except ValueError:
-            pass
+while True:
+    try:
+        uguess = int(input("Guess: "))
+    except ValueError:
+        pass
 
-     if uguess == guess:
+    if uguess == guess:
+        print
         print("Just right!")
+        break
     elif uguess > guess:
         print("Too large!")
         pass
@@ -23,4 +26,3 @@ while True:
         pass
 
 
-    break
