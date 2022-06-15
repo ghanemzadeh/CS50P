8259a04d5ac2a81b63d8d9c5a236dd0353ab661e
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-15 05:54:08 +0000

    Wed, Jun 15, 2022, 5:54 AM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -9,7 +9,8 @@ if len(sys.argv) == 3:
     if sys.argv[1] == "-f" or sys.argv[1] == "--font":
         f = sys.argv[2]
     else:
-        exit
+        print(Invalid usage)
+        sys.exit
 elif len(sys.argv) == 1:
     f = choice(figlet.getFonts())
 
