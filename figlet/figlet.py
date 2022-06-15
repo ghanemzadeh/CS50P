532a4348ba6fb82f47d507096b9d1f8c9a7fda77
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-15 05:39:39 +0000

    Wed, Jun 15, 2022, 5:39 AM +00:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -5,9 +5,9 @@ text = input("Input: ")
 
 if len(sys.argv) == 3:
     if sys.argv[1] == "-f" or sys.argv[1] == "--font":
-        figlet = Figlet(font=sys.argv[2])
-        figlet.setFont(font=f)
-        print(figlet.renderText(text))
+        f = sys.argv[2]
 elif len(sys.argv) == 1:
-    figlet = Figlet()
-    print(figlet.renderText(text))
+    
+
+figlet.setFont(font=f)
+print(figlet.renderText(text))
