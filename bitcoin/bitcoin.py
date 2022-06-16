b56e5a4ac3e84125ded2f52dba6151d1e1208fda
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-16 05:48:17 +0000

    Thu, Jun 16, 2022, 5:48 AM +00:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -1,8 +1,17 @@
 import requests
 
-try:
+if len(sys.argv) == 2:
+    print("Missing command-line argument")
+    sys.exit
+elif len(sys.argv) == 3:
+    try:
+        coins = float (sys.argv[2])
+    except:
+        print("Command-line argument is not a number")
+        sys.exit
+
+#try:
     ...
-except requests.RequestException:
+#except requests.RequestException:
 
 
-if len(sys.argv) == 3:
