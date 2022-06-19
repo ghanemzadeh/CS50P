3e116e95423c04ace5bb8e907bd5f63454750f7a
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-19 12:11:04 +0000

    Sun, Jun 19, 2022, 12:11 PM +00:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -4,7 +4,7 @@ from plates import is_valid
 
 def test_letters():
     assert value("CS50") == True
-    assert value("Hello, Newman") == 0
+    assert value("TP") == True
     assert value("How you doing?") == 20
     assert value("What's happening?") == 100
 
@@ -12,7 +12,7 @@ def test_length():
     assert value("Hello") == 0
 
 def test_numbers():
-    assert value("Hello") == 0
+    assert value("CS50P") == False
 
 def test_punctuation():
-    assert value("Hello") == 0
+    assert value("PI3.14") == False
