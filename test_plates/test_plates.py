f68c14723a11873bb4aa2e8627931846b058146b
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-19 12:15:44 +0000

    Sun, Jun 19, 2022, 12:15 PM +00:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -3,18 +3,18 @@ import pytest
 from plates import is_valid
 
 def test_letters():
-    assert value("CS50") == True
+    assert is_valid("CS50") == True
 
 
 def test_length():
-    assert value("TP") == True
-    assert value("H") == False
-    assert value("OUTATIME") == False
-    assert value("Abadan") == True
+    assert is_valid("TP") == True
+    assert is_valid("H") == False
+    assert is_valid("OUTATIME") == False
+    assert is_valid("Abadan") == True
 
 def test_numbers():
-    assert value("CS50P") == False
-    assert value("CS05") == False
+    assert is_valid("CS50P") == False
+    assert is_valid("CS05") == False
 
 def test_punctuation():
-    assert value("PI3.14") == False
+    assert is_valid("PI3.14") == False
