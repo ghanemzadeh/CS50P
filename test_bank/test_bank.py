7f0f801c613f4e6439fe4ed3f847223d2b23162c
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-19 09:28:11 +0000

    Sun, Jun 19, 2022, 9:28 AM +00:00

---
+++ test_bank/test_bank.py
@@ -0,0 +1,11 @@
+import pytest
+
+from bank import value
+
+
+def test_value():
+    assert value("Hello") == 0
+    assert value("Hello, Newman") == 0
+    assert value("How you doing?") == 20
+    assert value("This Is A Test") == "Ths s  Tst"
+
