d00ed9514ad6ebce6caf778260221ced9bd9dc34
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-21 11:38:28 +0000

    Tue, Jun 21, 2022, 11:38 AM +00:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -10,6 +10,8 @@ def main():
         except ZeroDivisionError:
             print("x/y, y should not be Zero")
             pass
+        except EOFError:
+                sys.exit()
 
 
 def convert(fraction):
