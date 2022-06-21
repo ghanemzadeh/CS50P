7c583d9bff9414aa1ddbf2d40d57f900fac67566
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-21 07:47:24 +0000

    Tue, Jun 21, 2022, 7:47 AM +00:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -3,7 +3,27 @@ def main():
 
 
 def convert(fraction):
-    ...
+    x, y = fraction.split("/")
+    x = int(x)
+    y = int(y)
+
+    percent = int(round(x/y*100))
+    if percent <= 1:
+           print("E")
+           break
+        elif 100 >= percent >= 90:
+            print("F")
+            break
+        elif percent > 100:
+            pass
+        else:
+            print (str(percent)+"%")
+            break
+    except ValueError:
+        print("x/y, x or y are not intger")
+        pass
+    except ZeroDivisionError:
+        pass
 
 
 def gauge(percentage):
