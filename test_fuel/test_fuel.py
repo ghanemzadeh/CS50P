0c0cafb745ded10c5f5a207f5dc108af7071bc34
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-21 10:15:43 +0000

    Tue, Jun 21, 2022, 10:15 AM +00:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -5,7 +5,9 @@ from fuel import convert, gauge
 def test_convert():
     assert convert("3/4") == 75
     assert convert("1/4") == 25
-    with pytest.raises(MissingBothCoordException):
->           sum_x_y(data)
 
 def test_gauge():
+    assert gauge(1) == "E"
+    assert gauge(90) == "F"
+    assert gauge(100) == "F"
+    assert gauge(80) == "80%"
