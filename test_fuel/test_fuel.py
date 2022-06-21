6d3805faa3010002e77f6018408c4f44ac2b2161
Nasser Ghanemzadeh <ghanemzadeh@gmail.com>
2022-06-21 10:30:22 +0000

    Tue, Jun 21, 2022, 10:30 AM +00:00

--- test_fuel/test_fuel.py
+++ test_fuel/test_fuel.py
@@ -5,6 +5,10 @@ from fuel import convert, gauge
 def test_convert():
     assert convert("3/4") == 75
     assert convert("1/4") == 25
+    try:
+        convert("1/0")
+    except ZeroDivisionError as exc:
+        assert False, f"Divide by zero Error {exc}"
 
 def test_gauge():
     assert gauge(1) == "E"
